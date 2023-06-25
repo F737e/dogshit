0bdd98689674357f8962b366192327b6c0fc3ac2
M Mahim miraj <85446878+F737e@users.noreply.github.com>
2023-06-25 06:52:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -18,31 +18,27 @@ def check_account_validation(mnemonic):
         account = Account.from_key(private_key)
         address = account.address
 
-        # Connect to first Infura API for account validation
-        infura_url_account = "https://mainnet.infura.io/v3/b08d259e4c874edb93d9f0025ffd461e"
-        w3_account = Web3(Web3.HTTPProvider(infura_url_account))
+        # Connect to Infura API for account validation
+        infura_url = "https://mainnet.infura.io/v3/b08d259e4c874edb93d9f0025ffd461e"
+        w3 = Web3(Web3.HTTPProvider(infura_url))
 
         # Perform account validation check
-        code = w3_account.eth.get_code(address)
+        code = w3.eth.get_code(address)
         return bool(code)  # True if account exists, False otherwise
     except ValidationError:
         return False  # Return False if mnemonic is not valid
 
-def check_account_balance(mnemonic):
-    private_key = Account.from_mnemonic(mnemonic)._private_key
-    account = Account.from_key(private_key)
-    address = account.address
-
-    # Connect to second Infura API for balance check
-    infura_url_balance = "https://mainnet.infura.io/v3/e1d702d09f394f5096e82c804e3d5c56"
-    w3_balance = Web3(Web3.HTTPProvider(infura_url_balance))
+def check_account_balance(address):
+    # Connect to Infura API for balance check
+    infura_url = "https://mainnet.infura.io/v3/e1d702d09f394f5096e82c804e3d5c56"
+    w3 = Web3(Web3.HTTPProvider(infura_url))
 
     # Get account balance
-    balance = w3_balance.eth.get_balance(address)
+    balance = w3.eth.get_balance(address)
     return balance
 
-def save_mnemonic(mnemonic):
-    with open("valid_mnemonics.txt", "a") as f:
+def save_mnemonic(mnemonic, file_name):
+    with open(file_name, "a") as f:
         f.write(mnemonic + "\n")
 
 def main():
@@ -51,16 +47,26 @@ def main():
         mnemonic = generate_mnemonic()
         print("Generated Mnemonic:", mnemonic)
 
-        # Check account validation
+        # Check if the account associated with the mnemonic exists
         if check_account_validation(mnemonic):
-            # Check account balance
-            balance = check_account_balance(mnemonic)
+            # Save the mnemonic to a file for further processing
+            save_mnemonic(mnemonic, "valid_mnemonics.txt")
+            print("Mnemonic saved at valid_mnemonics.txt")
+
+            # Retrieve the account address from the mnemonic
+            private_key = Account.from_mnemonic(mnemonic)._private_key
+            account = Account.from_key(private_key)
+            address = account.address
+
+            # Check the account balance
+            balance = check_account_balance(address)
             print("Account Balance:", balance)
 
-            # Save mnemonic if account balance is non-zero
+            # Save mnemonic to the recovery file if balance is non-zero
             if balance > 0:
-                save_mnemonic(mnemonic)
-                print("Mnemonic saved.")
+                save_mnemonic(mnemonic, "recovery_phase.txt")
+                print("Mnemonic saved at recovery_phase.txt")
 
 if __name__ == "__main__":
     main()
+
